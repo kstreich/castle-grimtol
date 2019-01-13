544d0d7241a917c5452382564ff62d8101b8726e
Kimberly Streicher <kimberlystreicher@gmail.com>
2019-01-12 22:38:06 -0700

    inventory

--- Project/GameService.cs
+++ Project/GameService.cs
@@ -109,7 +109,19 @@ namespace CastleGrimtol.Project
 
     public void Inventory()
     {
-      throw new System.NotImplementedException();
+      if (CurrentPlayer.Inventory.Count == 0)
+      {
+        System.Console.WriteLine("No items");
+      }
+      else
+      {
+        CurrentPlayer.Inventory.ForEach(item =>
+       {
+         System.Console.WriteLine(item.Name, item.Description);
+
+       });
+      }
+      GetUserInput();
     }
 
     public void Look()
@@ -137,11 +149,11 @@ namespace CastleGrimtol.Project
       Room room1 = new Room("Outside", "There is what looks to be, an abandoned mansion up a head (east).");
       Room room2 = new Room("Entry way", "Here you are amazed at the craftsmanship of the entry room. A grand stair case is a head of you (east) and to your north is the kitchen, while to the south is the living room. As you are admiring the room, you hear a swishing sound and a figure appears. The figure holding out three keys and wants you to only take one. Your first option is a brass skeleton key (key 1), second is a short stubby key (key 2), third is a jewel encrusted key (key 3). Take a key.");
       // Room room3 = new Room("Shed", "Wow, this shed is crazy old”, you mumble to yourself as you enter inside. But despite its apparent dilapidation there are a lot of neat things in it. Three of which are a hammer, a shovel, and an axe.");
-      Room room4 = new Room("Living Room", "Here you notice a finely furnished room, with a grand piano and a fully stocked bar");
-      Room room5 = new Room("Dining Room", "As you open the door to the dining room, the smell of freshly cooked food awakens your hunger. Along the grand dining table are various dishes of meats of all kinds, steamed and sautéed vegetables, breads and pastas, and anything else a person could imagine. To the right (east) is the door to the kitchen");
-      Room room6 = new Room("Kitchen", "As you walked into the kitchen you noticed how immaculate and clean it looked. Someone has to be here maintaining it, and who cooked all of the food? Out of impulse you yell, “Heellllooo, is anyone in here?");
-      Room room7 = new Room("Stairs", "The beautiful winding stair case looked to lead to a hallway of rooms. “Maybe you could find a safe place to stay the night?” you think to yourself. (Continue east, or west");
-      Room room8 = new Room("HallWay", "After climbing the stairs there are 3 doors (north, east, and south). You quickly shake the handles of all three doors, but they are locked. Then you remembered the key the ghost gave you upon entering the house…");
+      // Room room4 = new Room("Living Room", "Here you notice a finely furnished room, with a grand piano and a fully stocked bar");
+      // Room room5 = new Room("Dining Room", "As you open the door to the dining room, the smell of freshly cooked food awakens your hunger. Along the grand dining table are various dishes of meats of all kinds, steamed and sautéed vegetables, breads and pastas, and anything else a person could imagine. To the right (east) is the door to the kitchen");
+      // Room room6 = new Room("Kitchen", "As you walked into the kitchen you noticed how immaculate and clean it looked. Someone has to be here maintaining it, and who cooked all of the food? Out of impulse you yell, “Heellllooo, is anyone in here?");
+      // Room room7 = new Room("Stairs", "The beautiful winding stair case looked to lead to a hallway of rooms. “Maybe you could find a safe place to stay the night?” you think to yourself. (Continue east, or west");
+      Room room8 = new Room("HallWay", "Now there are 3 doors (north, east, and south). You quickly shake the handles of all three doors, but they are locked. Then you remembered the key the ghost gave you upon entering the house…");
       Room room9 = new Room("Bed Room 1", "Shocked that the key you have worked, you turn the handle. As the door is slowly opening, a very bright light is shining out of the room. The air is warm and smells sweet, like the beginning of spring. The light begins to dim and you recognize the surrounding garden as your home. In your cottage you see the silhouette of your beloved family.");
       Room room10 = new Room("Bed Room 2", "Shocked that the key you have worked, you turn the handle. As you push the door open, you get shoved into the room and the door instantly locks behind you. The room is completely dark, so much so that you can almost feel the darkness around you. You hear a low grumbling sound behind you. Turning to face the sound you see a pair of bring yellow eyes staring at you. The next moment you’ve been forced to the ground and the creature is ripping you apart with its razor sharp teeth");
       Room room11 = new Room("Bed Room 3", "Shocked that the key you have worked, you turn the handle. As you push the door open, you get shoved into the room and the door instantly locks behind you. The room is very dimly lit and you feel spider webs brushing against your face. As you push through the rooms the webs get thicker and thicker to where movement is being hindered. Before you know it, a spider the size of a human has you locked in its fangs.");
@@ -166,40 +178,40 @@ namespace CastleGrimtol.Project
       // room1.Exits.Add("west", room1W);
       room1.Exits.Add("east", room2);
 
-      room2.Exits.Add("north", room5);
-      room2.Exits.Add("south", room4);
+      // room2.Exits.Add("north", room5);
+      // room2.Exits.Add("south", room4);
       // room2.Exits.Add("west", room2W);
-      room2.Exits.Add("east", room7);
+      room2.Exits.Add("east", room8);
 
       // room3.Exits.Add("north", room1);
       // room3.Exits.Add("south", room3SWE);
       // room3.Exits.Add("west", room3SWE);
       // room3.Exits.Add("east", room3SWE);
 
-      room4.Exits.Add("north", room2);
+      // room4.Exits.Add("north", room2);
       // room4.Exits.Add("south", room3SWE);
       // room4.Exits.Add("west", room3SWE);
       // room4.Exits.Add("east", room3SWE);
 
       // room5.Exits.Add("north", room5NE);
-      room5.Exits.Add("south", room2);
+      // room5.Exits.Add("south", room2);
       // room5.Exits.Add("west", room5NE);
-      room5.Exits.Add("east", room6);
+      // room5.Exits.Add("east", room6);
 
       // room6.Exits.Add("north", room6NES);
       // room6.Exits.Add("south", room6NES);
-      room6.Exits.Add("west", room5);
+      // room6.Exits.Add("west", room5);
       // room6.Exits.Add("east", room6NES);
 
 
       // room7.Exits.Add("north", room7NS);
       // room7.Exits.Add("south", room7NS);
-      room7.Exits.Add("west", room2);
-      room7.Exits.Add("east", room8);
+      // room7.Exits.Add("west", room2);
+      // room7.Exits.Add("east", room8);
 
       room8.Exits.Add("north", room9);
       room8.Exits.Add("south", room11);
-      room8.Exits.Add("west", room7);
+      room8.Exits.Add("west", room2);
       room8.Exits.Add("east", room10);
 
       #endregion
@@ -244,13 +256,12 @@ namespace CastleGrimtol.Project
         return i.Name.ToUpper() == itemName.ToUpper();
       });
 
-      System.Console.WriteLine(item.Name);
-
       if (item != null)
       {
         CurrentRoom.Items.Remove(item);
         CurrentPlayer.Inventory.Add(item);
         System.Console.WriteLine($"{item.Name} was added to inventory");
+
       }
       else
       {
@@ -261,8 +272,12 @@ namespace CastleGrimtol.Project
     }
 
     public void UseItem(string itemName)
+    //No need to Pass a room since Items can only be used in the CurrentRoom
+    //Make sure you validate the item is in the room or player inventory before
+    //being able to use the item
     {
-      throw new System.NotImplementedException();
+
+
     }
   }
 }
